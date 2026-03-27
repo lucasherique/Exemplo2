aea60dfc59708b2c4acc5e603bf9dff093592585
Lucas <luca.henriqueas@gmail.com>
2026-03-27 09:50:44 -0300

    feat: declaracoes de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// DECLARAÇÕES E VARIÁVEIS
+// //VAR, LET E CONST
+
+var nome ="Fiap";
+console.log(nome);
+var nome ="Dev";
+console.log(nome);
+
+if(true) {
+    var avatar="Fiapinho";
+}
+
+console.log(avatar);
